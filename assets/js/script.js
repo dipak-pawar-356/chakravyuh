bbd395def774684be655a7fedda804a717dbea09
Dipak Pawar <dipakpawaras19@gmail.com>
2025-02-18 22:40:36 +0530

    add_ieee_logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,3 @@
-var swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  loop:true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 150,
-    modifier: 2.5,
-    slideShadows: true,
-  },
-  autoplay:{
-
-    delay:3000,
-    disableOnInteraction:false,
-  }
-
-});
 
 
 
